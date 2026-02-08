c33c1fb9c43afb302739d828c0ced9bb28a9d2eb
Ismael Jimenez <135388666+ismaeldjimenez@users.noreply.github.com>
2026-02-07 21:37:30 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function selectOption(option) {
         // Flash rainbow colors
         flashRainbowColors(function() {
             document.getElementById('question').style.display = 'none'; // Hide the question
-            displayCatHeart(); // Display the cat-heart.gif
+            displayCatHeart(); // Display the cat_kiss.gif
         });
     } else if (option === 'no') {
         // Change text on the "No" button to "You sure?"
@@ -56,19 +56,19 @@ function displayCat() {
     };
 }
 
-// Function to display the cat-heart.gif
+// Function to display the cat_kiss.gif
 function displayCatHeart() {
     // Clear existing content in the image container
     document.getElementById('image-container').innerHTML = '';
     // Get the container where the image will be displayed
     var imageContainer = document.getElementById('image-container');
-    // Create a new Image element for the cat-heart
+    // Create a new Image element for the cat_kiss
     var catHeartImage = new Image();
-    // Set the source (file path) for the cat-heart image
-    catHeartImage.src = 'cat-heart.gif'; // Assuming the cat-heart image is named "cat-heart.gif"
+    // Set the source (file path) for the cat_kiss image
+    catHeartImage.src = 'cat_kiss.gif'; // Assuming the cat_kiss image is named "cat_kiss.gif"
     // Set alternative text for the image (for accessibility)
     catHeartImage.alt = 'Cat Heart';
-    // When the cat-heart image is fully loaded, add it to the image container
+    // When the cat_kiss image is fully loaded, add it to the image container
     catHeartImage.onload = function() {
         imageContainer.appendChild(catHeartImage);
         // Hide the options container
