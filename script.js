097d1ee43bdb7479b769134062ceb07fcbafc315
Ismael Jimenez <135388666+ismaeldjimenez@users.noreply.github.com>
2026-02-07 22:33:08 -0700

    changed jpg to gif

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function displayCat() {
     // Create a new Image element for the cat
     var catImage = new Image();
     // Set the source (file path) for the cat image
-    catImage.src = 'cat_flower.jpg'; // Assuming the cat image is named "cat.jpg"
+    catImage.src = 'cat_flower.gif'; // Assuming the cat image is named "cat.jpg"
     // Set alternative text for the image (for accessibility)
     catImage.alt = 'Cat';
     // When the cat image is fully loaded, add it to the image container
